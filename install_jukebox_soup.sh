a19aac7fd0a331ed9812439c9f2e9ddeb8fc982e
lamtharnhantrakul <hanoi7@gmail.com>
2021-01-31 14:06:40 -0800

    docker dev working

--- install_jukebox_soup.sh
+++ install_jukebox_soup.sh
@@ -6,7 +6,9 @@ git config --global user.name "lamtharnhantrakul"
 /usr/bin/python3 -m pip install --upgrade pip
 sudo apt-get -y install python3-pip
 
-pip3 install git+https://github.com/openai/jukebox.git
+pip install git+https://github.com/openai/jukebox.git
 
 sudo apt-get update -y
 sudo apt-get install -y libsndfile-dev
+
+pip install torch torchvision
